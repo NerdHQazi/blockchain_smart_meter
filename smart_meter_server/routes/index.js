ee78503d1008598a8bce5ba7c9f7ec624b1f6704
Olatubosun John <johntubson@gmail.com>
2024-05-13 15:43:00 +0100

    refactor port to enable hosting

--- smart_meter_server/routes/index.js
+++ smart_meter_server/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/api', function (req, res, next) {
+  let bodyRequest = req.body;
+  res.send(JSON.stringify(bodyRequest));
+})
 
 module.exports = router;
